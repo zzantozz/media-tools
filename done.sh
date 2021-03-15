fe19f2e78f208d374a33f667f4f9214034f12e58
ryan <zzantozz@gmail.com>
2021-03-14 21:07:33 -0500

    done.sh supports multi-level paths for log files
    For processing movies, each movie will be in a separate directory for
    input, and outputs will sometimes be in the nested plex directory
    structure. To support this, the done script needs to be able to
    understand file names that aren't just in a flat directory. This keeps
    the original behavior for backward compatibility and adds the ability
    to pass a relative path for using for the log lookup.

--- done.sh
+++ done.sh
@@ -5,24 +5,28 @@
 [ $# -eq 2 ] || {
 	echo "Checks if an mkv file has been encoded by examining a log file of ffmpeg output."
 	echo "Takes two args:"
-	echo "1. The name of an mkv file"
+	echo "1. The name or path of an mkv file"
 	echo "2. The path to a directory containing log files"
 	echo ""
-	echo 'The log files should be named "$(basename "$1").log" so they can be correlated to'
-	echo "the input file."
+	echo 'Returns 0 if a log file named "$1.log" or "$(basename "$1").log" exists and looks'
+	echo 'looks like a finished ffmpeg encode. Returns 1 otherwise.'
 	exit 1
 }
 
 BASENAME=$(basename "$1")
-LOGFILE="$2/$BASENAME.log"
+LOGFILE1="$2/$BASENAME.log"
+LOGFILE2="$2/$1.log"
 
-[ -f "$LOGFILE" ] || {
-	echo "Expected log file doesn't exist for checking doneness:"
-	echo "$LOGFILE"
+[ -f "$LOGFILE1" ] && FILE="$LOGFILE1"
+[ -f "$LOGFILE2" ] && FILE="$LOGFILE2"
+[ -n "$FILE" ] || {
+	echo "Couldn't find log file to check doneness. Expected one of:" >&2
+	echo "- $LOGFILE1" >&2
+	echo "- $LOGFILE2" >&2
 	exit 1
 }
 
 # x265 ends with an "encoded ..." line, x264 with a "[libx264 @..." line
-tail -1 "$LOGFILE" | grep "^encoded" &>/dev/null || tail -1 "$LOGFILE" | grep '^\[libx264 @.*kb/s:' &>/dev/null && exit 0
+tail -1 "$FILE" | grep "^encoded" &>/dev/null || tail -1 "$FILE" | grep '^\[libx264 @.*kb/s:' &>/dev/null && exit 0
 exit 1
 
