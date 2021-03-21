8e08c2b4c47e4ffc622769e49b9edeca85e597d5
ryan <zzantozz@gmail.com>
2021-03-21 17:53:20 -0500

    formatting and better debugging for interlacing script
    I just wanted more informative output from this script and ended up
    fixing the formatting while I was there.

--- interlaced.sh
+++ interlaced.sh
@@ -1,10 +1,10 @@
 #!/bin/bash -e
 
 [ $# -eq 1 ] || {
-	echo "Pass a video file to check if it's interlaced." >&2
-	echo "Prints 'interlaced' if interlaced." >&2
-	echo "Prints 'progressive' if progressive." >&2
-	exit 1
+    echo "Pass a video file to check if it's interlaced." >&2
+    echo "Prints 'interlaced' if interlaced." >&2
+    echo "Prints 'progressive' if progressive." >&2
+    exit 1
 }
 
 function debug {
@@ -27,20 +27,22 @@ RE1="Single frame detection: TFF: *([[:digit:]]+) *BFF: *([[:digit:]]+) *Progres
 RE2="Multi frame detection: TFF: *([[:digit:]]+) *BFF: *([[:digit:]]+) *Progressive: *([[:digit:]]+) *Undetermined: *([[:digit:]]+)"
 
 [[ $DATA =~ $RE1 ]] && {
-	TFF1="${BASH_REMATCH[1]}" && BFF1="${BASH_REMATCH[2]}" && PROG1="${BASH_REMATCH[3]}" && UNDET1="${BASH_REMATCH[4]}"
+    TFF1="${BASH_REMATCH[1]}" && BFF1="${BASH_REMATCH[2]}" && PROG1="${BASH_REMATCH[3]}" && UNDET1="${BASH_REMATCH[4]}"
 } || {
-	echo "No match on single frame detection" >&2
-	exit 1
+    echo "No match on single frame detection" >&2
+    exit 1
 }
 debug "tff: $TFF1 bff: $BFF1 progressive: $PROG1 undetermined: $UNDET1"
 
 [[ $DATA =~ $RE2 ]] && {
-	TFF2="${BASH_REMATCH[1]}" && BFF2="${BASH_REMATCH[2]}" && PROG2="${BASH_REMATCH[3]}" && UNDET2="${BASH_REMATCH[4]}"
+    TFF2="${BASH_REMATCH[1]}" && BFF2="${BASH_REMATCH[2]}" && PROG2="${BASH_REMATCH[3]}" && UNDET2="${BASH_REMATCH[4]}"
 } || {
-	echo "No match on multi frame detection" >&2
-	exit 1
+    echo "No match on multi frame detection" >&2
+    exit 1
 }
 debug "tff: $TFF2 bff: $BFF2 prog: $PROG2 undetermined: $UNDET2"
+
+VALUES="Single frame: tff=$TFF1 bff=$BFF1 progressive=$PROG1 undetermined=$UNDET1\nDouble frame: tff=$TFF2 bff=$BFF2 prog=$PROG2 undetermined=$UNDET2"
 
 # If progressive is in the hundreds and T/BFF's are in the single
 # digits, it's progressive, regardless of the undetermined frames.
@@ -54,12 +56,14 @@ debug "tff: $TFF2 bff: $BFF2 prog: $PROG2 undetermined: $UNDET2"
 
 # If undetermined is the highest number, abort
 [ $UNDET1 -gt $TFF1 ] && [ $UNDET1 -gt $BFF1 ] && [ $UNDET1 -gt $PROG1 ] && {
-	echo "Single frame detection was undetermined" >&2
-	exit 1
+    echo "Single frame detection was undetermined" >&2
+    echo -e "$VALUES" >&2
+    exit 1
 }
 [ $UNDET2 -gt $TFF2 ] && [ $UNDET2 -gt $BFF2 ] && [ $UNDET2 -gt $PROG2 ] && {
-	echo "Multi frame detection was undetermined" >&2
-	exit 1
+    echo "Multi frame detection was undetermined" >&2
+    echo -e "$VALUES" >&2
+    exit 1
 }
 
 # If top- or bottom-frame-first numbers are bigger, it's interlaced. If not, progressive.
@@ -68,7 +72,8 @@ debug "tff: $TFF2 bff: $BFF2 prog: $PROG2 undetermined: $UNDET2"
 
 # If single and multi disagree, abort
 [ $ANSWER1 = $ANSWER2 ] || {
-	echo "Got different answers from single and multi" >&2
-	exit 1
+    echo "Got different answers from single and multi" >&2
+    echo -e "$VALUES" >&2
+    exit 1
 }
 echo $ANSWER1
