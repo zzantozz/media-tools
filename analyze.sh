f0570f97fc0a1138081d8181df4859d4ce503935
ryan <zzantozz@gmail.com>
2021-10-05 19:21:10 -0500

    analyze.sh caches its findings
    The analyze script is great but can run a long time in some
    cases. Caching its output is useful when iterating on encodings trying
    to find proper cut ranges.

--- analyze.sh
+++ analyze.sh
@@ -12,22 +12,33 @@ function debug {
 }
 
 MYDIR="$(dirname "$0")"
+CACHEDIR="cache/analyze"
+MOVIEDIR="$(basename "$(dirname "$1")")"
+BASENAME="$(basename "$1")"
+CACHEFILE="$CACHEDIR/$MOVIEDIR/$BASENAME"
+[ -f "$CACHEFILE" ] && {
+    debug "Found cached data in '$CACHEFILE'"
+    cat "$CACHEFILE"
+    exit 0
+}
+debug "No cached data found, analyzing input file"
 TMP="$(mktemp)"
-TMPDIR="$(mktemp -d)"
-debug "Sampling input..."
 debug " - $(date)"
 $MYDIR/sample.sh "$1" 30 | ffmpeg -y -i pipe: -filter 'idet,cropdetect=round=2' -f null /dev/null &> "$TMP"
 debug " - $(date)"
 
+mkdir -p "$(dirname "$CACHEFILE")"
 INTERLACED=$("$MYDIR/interlaced.sh" -i "$TMP" -f output) && {
     debug "Interlaced: $INTERLACED"
     echo "INTERLACED=$INTERLACED"
+    echo "INTERLACED=$INTERLACED" >> "$CACHEFILE"
 } || {
     debug "Interlace detection failed, but continuing, since it could be set manually"
 }
 CROPPING=$("$MYDIR/crop.sh" -i "$TMP" -f output) && {
     debug "Cropping: $CROPPING"
     echo "CROPPING=$CROPPING"
+    echo "CROPPING=$CROPPING" >> "$CACHEFILE"
 } || {
     debug "Crop detect failed, but continuing, since it could be set manually"
 }
