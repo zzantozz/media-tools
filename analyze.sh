cf69a3d9860f82bd3db203f86792ab5d029345de
ryan <zzantozz@gmail.com>
2022-04-03 22:29:44 -0500

    adds "use cache" option to analyze script
    I needed to troubleshoot the script, and it had already cached a
    result. Rather than having to delete the cache file and rerun the
    script over and over, I opted to add a variable that lets you skip
    using the cache at all.

--- analyze.sh
+++ analyze.sh
@@ -16,30 +16,31 @@ CACHEDIR="cache/analyze"
 MOVIEDIR="$(basename "$(dirname "$1")")"
 BASENAME="$(basename "$1")"
 CACHEFILE="$CACHEDIR/$MOVIEDIR/$BASENAME"
-[ -f "$CACHEFILE" ] && {
+USECACHE=${USECACHE:-true}
+if [ -f "$CACHEFILE" ] && [ "$USECACHE" = true ]; then
     debug "Found cached data in '$CACHEFILE'"
     cat "$CACHEFILE"
     exit 0
-}
+fi
 debug "No cached data found, analyzing input file"
 TMP="$(mktemp)"
 debug " - $(date)"
 $MYDIR/sample.sh "$1" 30 | ffmpeg -y -i pipe: -filter 'idet,cropdetect=round=2' -f null /dev/null &> "$TMP"
 debug " - $(date)"
 
-mkdir -p "$(dirname "$CACHEFILE")"
-INTERLACED=$("$MYDIR/interlaced.sh" -i "$TMP" -f output) && {
+[ $USECACHE = true ] && mkdir -p "$(dirname "$CACHEFILE")"
+if INTERLACED=$("$MYDIR/interlaced.sh" -i "$TMP" -f output); then
     debug "Interlaced: $INTERLACED"
     echo "INTERLACED=$INTERLACED"
-    echo "INTERLACED=$INTERLACED" >> "$CACHEFILE"
-} || {
+    [ $USECACHE = true ] && echo "INTERLACED=$INTERLACED" >> "$CACHEFILE"
+else
     debug "Interlace detection failed, but continuing, since it could be set manually"
-}
-CROPPING=$("$MYDIR/crop.sh" -i "$TMP" -f output) && {
+fi
+if CROPPING=$("$MYDIR/crop.sh" -i "$TMP" -f output); then
     debug "Cropping: $CROPPING"
     echo "CROPPING=$CROPPING"
-    echo "CROPPING=$CROPPING" >> "$CACHEFILE"
-} || {
+    [ $USECACHE = true ] && echo "CROPPING=$CROPPING" >> "$CACHEFILE"
+else
     debug "Crop detect failed, but continuing, since it could be set manually"
-}
+fi
 rm "$TMP"
