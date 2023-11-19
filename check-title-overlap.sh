e85a69853e403408f3917269c643aa4756f6b965
zzantozz <zzantozz@gmail.com>
2023-11-19 16:06:17 -0600

    add script to find title overlap
    One of the hardest things in ripping stuff is going through all the titles of a disk to figure
    out what's what. One of the most annoying things is when you find title that overlap with others.
    Sometimes it's an exact duplicate without chapter marks. Sometimes several titles are parts of
    another, aggregate title. This adds a script to help find those overlaps before the disk is
    even ripped. It can look at makemkvcon info output and compare title segments to help understand
    how titles are interrelated.

---
+++ check-title-overlap.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+# Checks MakeMKV info output to find titles with overlapping segments.
+# 
+# For example, often a DVD or Bluray will have one title containing all the deleted scenes but also have
+# every deleted scene as its own title. This will find things like that so you can easily identify what
+# to do with them.
+#
+# Call with the output of 'makemkvcon -r info disc:0'. You can provide the output either on stdin or by
+# calling this script with the name of a file that contains the output.
+
+contains() { arr=($1); item="$2"; for x in "${arr[@]}"; do if [ "$x" = "$item" ]; then return 0; fi; done; return 1; }
+contains_all() { items=($2); for item in "${items[@]}"; do if ! contains "$1" "$item"; then return 1; fi; done; return 0; }
+
+segment_map=()
+while read -r line; do
+  if [[ "$line" =~ ^TINFO:(.+),26,0,\"(.*)\"$ ]]; then
+    title="${BASH_REMATCH[1]}"
+    segments=$(echo "${BASH_REMATCH[2]}" | tr , ' ')
+    segment_map["$title"]="$segments"
+  fi
+done < "${1:-/dev/stdin}"
+
+for title in "${!segment_map[@]}"; do
+  for k in "${!segment_map[@]}"; do
+    if [ "$k" = "$title" ]; then
+      continue
+    fi
+    if [ "${segment_map[$k]}" = "${segment_map[$title]}" ]; then
+      echo "title $title is the same as title $k"
+    elif contains_all "${segment_map[$k]}" "${segment_map[$title]}"; then
+      echo "title $title is fully contained by title $k"
+    fi
+  done
+done
