7a182358acaf23a0e29ef258e44a6243d6187b4a
zzantozz <zzantozz@gmail.com>
2025-04-02 20:31:35 -0500

    adapt quality.sh and sample.sh to 4k
    quality.sh script needs to accommodate 4k resolution to determine
    the right encoding quality.
    
    sample.sh has to sample a lot less because the ffmpeg command there
    buffers in memory, and with 4k, even 20G wasn't enough at 120 chunks!
    I'd like to find a better way to do the sampling that doesn't buffer,
    so that I can go back to a higher chunk count.

--- quality.sh
+++ quality.sh
@@ -7,10 +7,17 @@
 }
 
 DATA=$(ffprobe -i "$1" 2>&1 | grep "Stream #0:0")
+
+if [[ "$DEBUG" =~ quality ]]; then
+  echo "ffprobe output:" >&2
+  echo "$DATA" >&2
+fi
+
 [[ $DATA =~ 720x480 ]] && Q=22
 [[ $DATA =~ 1280x720 ]] && Q=22
 [[ $DATA =~ 1918x1080 ]] && Q=20
 [[ $DATA =~ 1920x1080 ]] && Q=20
+[[ $DATA =~ 3840x2160 ]] && Q=20
 [ -z "$Q" ] && {
 	echo "Can't figure out what video quality to use." >&2
 	exit 1
