b2c5ef16acff0c0326d5ade15c3f5163ef2e51f1
zzantozz <zzantozz@localhost>
2026-02-12 00:36:16 -0600

    tweak movie/tv detection
    This was making an extra from BSG Razor to be categorized as tv because
    it was named '/Other/Season 4 Sneak Peek.mkv'. Since it has "Season" in
    the name, the script thought it belonged in the tv-shows dir.

--- allinone/encode.sh
+++ allinone/encode.sh
@@ -222,9 +222,11 @@ function encode_one {
     return 1
   fi
 
-  # Also check for season in the output path! If the disks aren't ripped into the typical "tv show" structure,
-  # but have an OUTPUTNAME that puts the outputs in that structure, the output would have a season in it.
-  if [ -z "$SEASON" ] && [[ "$output_rel_path" =~ $season_regex ]]; then
+  # Also check for season in the output path! If the disks aren't ripped into the typical "tv show" structure, but have
+  # an OUTPUTNAME that puts the outputs in that structure, the output would have a season in it. Check with a more
+  # strict regex here. Outputs should always look like "<show name>/Season N+/episode.mkv". Using the looser regex above
+  # has wrongly identified a special feature that had "Season" in the name.
+  if [ -z "$SEASON" ] && [[ "$output_rel_path" =~ /[Ss]eason\ [[:digit:]]*/ ]]; then
     local SEASON="${BASH_REMATCH[1]}"
   fi
 
