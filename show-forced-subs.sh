22b03d05c3bf992e84102feb167447a457b7aeb0
Ryan Stewart <ryan.stewart@rackspace.com>
2026-02-16 13:34:46 -0600

    forced sub audit mode
    Add to the show-forced-subs script to make it able to compare desired
    state against actual. Now it can either find titles that it thinks
    should have forced subs and show them, or it can go an extra step
    and see what's in them now, indicating whether the sub it detected
    is currently forced or not.

--- show-forced-subs.sh
+++ show-forced-subs.sh
@@ -18,6 +18,9 @@ usage() {
   echo "    -h"
   echo "        Show this usage info and exit."
   echo
+  echo "    -a"
+  echo "        Audit mode. After determining if a title *should* have forced subs, check if it does."
+  echo
   echo "    -s"
   echo "        Show summary table. Writes final lists of inputs that do and do not have forced subs. Useful for"
   echo "        sending to an AI to see what it thinks of the results."
@@ -27,11 +30,14 @@ usage() {
   echo "        where the subs occur in the input."
 }
 
-while getopts "hts" opt; do
+while getopts "hats" opt; do
   case "$opt" in
     h)
       usage
       exit 0
+      ;;
+    a)
+      audit=true
       ;;
     t)
       show_frame_times=true
@@ -50,21 +56,53 @@ without=()
 
 while read -r line; do
   abs_path="$line"
-  subs="$(./forced-subs.sh -i "$abs_path")"
-  printf ' -> '
-  if [ "$subs" = none ] || [ -z "${subs//[[:blank:]]/}" ]; then
-    printf "$red"
-    without+=("$abs_path")
-  else
-    printf "$green"
+  desired_forced="$(./forced-subs.sh -i "$abs_path")"
+  if [[ "$desired_forced" =~ [0-9][0-9]* ]]; then
+    should_force=true
     with+=("$abs_path")
+  else
+    should_force=false
+    without+=("$abs_path")
   fi
-  printf "%s -- %s\n" "$abs_path" "$subs"
+  printf ' -> '
+  if [ "$audit" = true ]; then
+    # Get forced status of sub streams. Example:
+    # 5|0
+    # 6|1
+    # 7|0
+    ffprobe -v error -select_streams s -show_entries stream=index:disposition=forced -of compact=p=0:nk=1 "$abs_path" | \
+    while read -r line; do
+      IFS='|' read -r -a args <<<"$line"
+      stream_index="${args[0]}"
+      forced="${args[1]}"
+      if [ "$forced" = 1 ]; then
+        if [ -n "$existing_forced_stream" ]; then
+            die "Found two existing forced streams. Not sure what to do! All forced dispositions: ${lines[*]}"
+        fi
+        existing_forced_stream="$stream_index"
+      fi
+    done
+    # Check if the suspected forced stream is the same as the existing one
+    case "$should_force:$existing_forced_stream" in
+      # Good if the one we want is already forced, or if we don't want anything forced and it isn't
+      true:$desired_forced|false:) printf "$green" ;;
+      # Bad if anything else
+      *) printf "$red" ;;
+    esac
+  else
+    # Not audit mode, just highlight the files that we think should have forced subs
+    if [ "$should_force" = true ]; then
+      printf "$green"
+    else
+      printf "$red"
+    fi
+  fi
+  printf "%s -- %s\n" "$abs_path" "$desired_forced"
   printf "$nc"
-  if [ "$show_frame_times" = true ] && [[ "$subs" =~ [0-9][0-9]* ]]; then
+  if [ "$show_frame_times" = true ] && [[ "$desired_forced" =~ [0-9][0-9]* ]]; then
     # Show dts_time of packets in the stream in csv format, which puts it in a single line. I have no idea what the
     # read_intervals option does.
-    ffprobe -v error -select_streams "$subs" -read_intervals 0%+#11 -show_entries packet=dts_time -of csv=p=0 "$abs_path" | head -5
+    ffprobe -v error -select_streams "$desired_forced" -read_intervals 0%+#11 -show_entries packet=dts_time -of csv=p=0 "$abs_path" | head -5
   fi
 done
 
