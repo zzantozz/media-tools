acbb83b7824594725c714b3571090f095249a6e8
Ryan Stewart <ryan.stewart@rackspace.com>
2026-02-16 19:56:40 -0600

    util to convert seconds to readable time
    I've been putting this in google to interpret the timestamps of
    subtitle frames while looking for forced subs. It's way more
    convenient to be able to convert on the fly.

---
+++ seconds-to-time.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+. allinone/utils
+
+seconds="$1"
+
+[ -z "$seconds" ] && read -r seconds
+
+[ -n "$seconds" ] || die "Pass seconds to convert as one and only arg, or on stdin"
+
+result="$(secs_to_duration "$seconds")"
+
+echo " -> $result"
