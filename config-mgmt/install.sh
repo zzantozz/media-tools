a605a139f01b9fcd9a7e135142272beb94be0db5
ryan <zzantozz@gmail.com>
2026-08-29 05:59:12 +0000

    more install updates
    The install script wasn't installing docker. This gets it to where it
    seems like it'll run successfully on ubuntu server 26.04.1. I ran all
    the commands manually, but I'd already installed other things. May
    need to tweak more next time.

--- config-mgmt/install.sh
+++ config-mgmt/install.sh
@@ -1,9 +1,29 @@
 #!/bin/bash
 
+# Official docker setup to get access to compose
+sudo install -m 0755 -d /etc/apt/keyrings
+sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
+sudo chmod a+r /etc/apt/keyrings/docker.asc
+
+# Add the repository to Apt sources:
+sudo tee /etc/apt/sources.list.d/docker.sources <<EOF
+Types: deb
+URIs: https://download.docker.com/linux/ubuntu
+Suites: $(. /etc/os-release && echo "${UBUNTU_CODENAME:-$VERSION_CODENAME}")
+Components: stable
+Architectures: $(dpkg --print-architecture)
+Signed-By: /etc/apt/keyrings/docker.asc
+EOF
+
 # Installs the things I need for everything here to work (on Ubuntu)
 # Which of these things installed postfix??
-#
+sudo apt update
 sudo apt -y install ffmpeg cifs-utils git iotop nmap sqlite3 emacs
+# packages recommended by docker - are all these necessary?
+sudo apt -y install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+
+sudo usermod -aG docker $USER
+
 sudo update-alternatives --set editor "$(which emacs)"
 echo "alias em=emacs" >>~/.bashrc
 for mount in d j k l plex-media; do
@@ -39,4 +59,11 @@ echo ""
 echo "and then secure it:"
 echo ""
 echo "chmod 400 ~/smbcreds"
+echo ""
+echo "Also, this script has modified init files and user permissions. It would be best to log out and in now."
+echo ""
+echo "Finally, if all has gone as planned, you should be able to build an image with ffmpeg and encode with it:"
+echo ""
+echo "docker build -t ffmpeg-for-server ."
+echo "rm -f allinone/stop; docker compose down; docker compose up --scale ffmpeg=1 -d; docker compose logs -f"
 echo ""
